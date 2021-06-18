020b43b203055defa56f8e5b319981e1c44a7d9c
milkcandy1 <85884146+milkcandy1@users.noreply.github.com>
2021-06-18 16:05:34 +0100

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,66 +7,74 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
 module top_tb(
-	);
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
 
-parameter CLK_PERIOD = 10; 
-
+//Todo: Regitsers and wires
 	reg clk;
 	reg [4:0] temperature;
 	reg err;
 	wire heating;
 	wire cooling;
 
-	initial 
+//Todo: Clock generation
+	initial
 	begin
 		clk = 1'b0;
 	forever
 		#(CLK_PERIOD/2) clk=~clk;
 	end
 
+//Todo: User logic
 	initial begin
-		temperature=5'd5;
+		temperature=5'd5; // testing starting with 17?
 		err=0;
-
+		
 		forever begin
 			#CLK_PERIOD
+				
+			if ((heating & (temperature>=5'd20))|(!heating & (temperature<=5'd18))) // test heating
+				begin
+				$display("***TEST FAILED! not the right state!***", temperature, heating, cooling);
+				err=1;
+				end
 
-		if ((heating & (temperature>=5'd20))|(!heating & (temperature<=5'd18)))
-			begin
-			$display("***TEST FAILED! Wrong state!***", temperature, heating, cooling);
-			err=1;
-			end
-
-		if ((cooling & (temperature<=5'd20))|(!cooling & (temperature>=5'd22))) 
-			begin
-			$display("***TEST FAILED! Wrong state!***", temperature, heating, cooling);
-			err=1;
-			end
-	
-		temperature = temperature+1'd1;
-		if (temperature>5'd30)
-		temperature=5'd5;
-
+			if ((cooling & (temperature<=5'd20))|(!cooling & (temperature>=5'd22))) // test cooling 
+				begin
+				$display("***TEST FAILED! not the right state!***", temperature, heating, cooling);
+				err=1;
+				end
+			
+			temperature=temperature+1'd1;
+			if (temperature>=5'd30)
+				temperature=5'd5;
+			
 		end
 	end
 
+
+//Todo: Finish test, check for success
 	initial begin
 	#(500*CLK_PERIOD)
-	if (err==0)
-		$display ("***TEST PASSED!***");
-		$finish;
-
-	end
-
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+      
+//Todo: Instantiate counter module
 	aircond aircond1 (
-	.temperature (temperature),
+	.temperature (temperature), 
 	.clk (clk),
 	.heating (heating),
 	.cooling (cooling)
 	);
-
-endmodule
+ 
+endmodule 
 
 
 
